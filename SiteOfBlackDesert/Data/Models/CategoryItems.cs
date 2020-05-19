dfe9db18823127ba2df115df5d91bcf6ffd688f7
Prankanyl <v752433@icloud.com>
2020-05-20 01:25:55 +0300

    Patch v.1.0

---
+++ SiteOfBlackDesert/Data/Models/CategoryItems.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiteOfBlackDesert.Data.Models
+{
+    class CategoryItems
+    {
+        public int id { set; get; }
+        public string categoryitemsname { set; get; }
+        public string descriptions { set; get; }
+        //public int id { set; get; }
+    }
+}
---
+++ SiteOfBlackDesert/Data/Models/Items.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiteOfBlackDesert.Data.Models
+{
+    class Items
+    {
+        public int id { set; get; }
+        public string category { set; get; }
+        public string name { set; get; }
+        public string descriptions { set; get; }
+        public string img { set; get; }
+        public int  CategoryItemsID { set; get; }
+        public virtual CategoryItems CategoryItems { set; get; }
+    }
+}
